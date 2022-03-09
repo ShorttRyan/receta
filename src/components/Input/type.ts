4078f5b4461a289ca184a3f746e86c631b0531e2
ryanepshortt <ryan.ep.shortt@gmail.com>
2022-03-08 20:23:48 -0500

    login form work

---
+++ src/components/Input/type.ts
@@ -0,0 +1,5 @@
+export type FormInput<InputType> = {
+  value: InputType
+  error: boolean
+  message?: string
+}
--- src/components/LoginSignup/Login/index.tsx
+++ src/components/LoginSignup/Login/index.tsx
@@ -1,56 +1,82 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './Login.module.scss'
 import Input from '../../Input'
 import Button from '../../Button'
 import { logIn } from '../../../API/auth'
 import { useRouter } from 'next/router'
-
-interface LoginForm {
-  username: string
-  password: string
-}
+import { initialValue, LoginForm } from './type'
 
 interface LoginProps {
   hide: boolean
 }
 
 const Login: React.FunctionComponent<LoginProps> = ({ hide }) => {
-  const [credentials, setCredentials] = useState<LoginForm>({
-    username: '',
-    password: '',
-  })
+  const [credentials, setCredentials] = useState<LoginForm>(initialValue)
   const router = useRouter()
+  useEffect(() => {
+    setCredentials({
+      username: { value: '', error: false },
+      password: { value: '', error: false },
+    })
+  }, [hide])
   return (
     <form
       className={hide ? styles.hidden : styles.loginWrapper}
       onSubmit={async (e) => {
         e.preventDefault()
-        const [result, error] = await logIn(credentials)
-        console.log(result, error)
+        const newCredentials = { ...credentials }
+        newCredentials.username.error = false
+        newCredentials.password.error = false
+        setCredentials(newCredentials)
+        const [result, error] = await logIn({
+          username: credentials.username.value,
+          password: credentials.password.value,
+        })
         if (error === undefined) {
           router.push('/')
+        } else {
+          const newCredentials = { ...credentials }
+          newCredentials.username.error = true
+          newCredentials.password.error = true
+          setCredentials(newCredentials)
         }
       }}
     >
       <Input
-        name="Username/Email"
+        name="Username / Email"
         id="loginUsername"
         placeholder="Username or Email"
         type="input"
-        value={credentials.username}
-        setValue={(newVal: string) =>
-          setCredentials({ ...credentials, username: newVal })
-        }
+        error={credentials.username.error}
+        value={credentials.username.value}
+        setValue={(newVal: string) => {
+          const newCredentials = { ...credentials }
+          newCredentials.username.value = newVal
+          setCredentials(newCredentials)
+        }}
+        onBlur={() => {
+          const newCredentials = { ...credentials }
+          newCredentials.username.error = false
+          setCredentials(newCredentials)
+        }}
       />
       <Input
         name="Password"
         id="loginPassword"
         placeholder="Password"
         type="password"
-        value={credentials.password}
-        setValue={(newVal: string) =>
-          setCredentials({ ...credentials, password: newVal })
-        }
+        error={credentials.password.error}
+        value={credentials.password.value}
+        setValue={(newVal: string) => {
+          const newCredentials = { ...credentials }
+          newCredentials.password.value = newVal
+          setCredentials(newCredentials)
+        }}
+        onBlur={() => {
+          const newCredentials = { ...credentials }
+          newCredentials.password.error = false
+          setCredentials(newCredentials)
+        }}
       />
       <div className={styles.submitWrapper}>
         <Button label="Submit" type="submit" />
