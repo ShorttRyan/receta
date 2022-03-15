ba1e83b2373ede3a7571b716529bcc980d620b67
ryanepshortt <ryan.ep.shortt@gmail.com>
2022-03-15 17:07:06 -0400

    adding navbar

--- src/pageComponents/HomeContent/index.tsx
+++ src/pageComponents/HomeContent/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import styles from './HomeContent.module.scss'
 
 const HomeContent: React.FunctionComponent = () => {
-  return <div>Home Content</div>
+  return (
+    <>
+      <div className={styles.subNavWrapper}>
+        <div className={styles.subNavLinkWrapper}>
+          <button className={styles.subNavLink}>My Recipes</button>
+          <button className={styles.subNavLink}>Liked Recipes</button>
+        </div>
+      </div>
+      <div>Home Content</div>
+    </>
+  )
 }
 
 export default HomeContent
